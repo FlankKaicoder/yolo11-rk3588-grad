55ecd17a57eda14106916c3f7a51dbc6efa9b6e4
lkkknnn <18693819616@163.com>
2026-03-10 23:29:29 +0800

    add simam attention on p2 branch

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2139,3 +2139,21 @@ class CBAM(nn.Module):
         x = x * self.channel_attention(x)
         x = x * self.spatial_attention(x)
         return x
+class SimAM(nn.Module):
+    """Simple, parameter-free attention module."""
+
+    def __init__(self, c1=None, e_lambda=1e-4):
+        super().__init__()
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        n = h * w - 1 if h * w > 1 else 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (
+            4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)
+        ) + 0.5
+
+        return x * self.activaton(y)
